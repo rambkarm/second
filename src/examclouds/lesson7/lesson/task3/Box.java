1c8e6654a71082ce44378490d7b4e1a333a5446e
rambkarm <rafaildolinskiy@gmail.com>
2022-12-01 23:29:04 +0200

    lesson 7 task 3. array and loop was added

--- src/examclouds/lesson7/lesson/task3/Box.java
+++ src/examclouds/lesson7/lesson/task3/Box.java
@@ -1,7 +1,60 @@
 package examclouds.lesson7.lesson.task3;
 
 public class Box {
-    double width;
-    double height;
-    double depth;
+    private double width;
+    private double height;
+    private double depth;
+
+    public Box(double width, double height, double depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public Box() {
+    }
+
+    public double getVolume() {
+        return width * height * depth;
+    }
+
+    public void setData(double a, double b, double c) {
+        width = a;
+        height = b;
+        depth = c;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "width=" + width +
+                ", height=" + height +
+                ", depth=" + depth +
+                ", volume=" + getVolume() +
+                '}';
+    }
 }
--- src/examclouds/lesson7/lesson/task3/DemoBox1.java
+++ src/examclouds/lesson7/lesson/task3/DemoBox1.java
@@ -3,11 +3,18 @@ package examclouds.lesson7.lesson.task3;
 public class DemoBox1 {
     public static void main(String[] args) {
         Box box1 = new Box();
-
-        double a = box1.depth = 78.88;
-        double b = box1.height = 56.79789;
-        double c = box1.width = 456.77;
-        double volume = a * b * c;
-        System.out.println(volume);
+        Box box2 = new Box(3.3, 4.4, 5.5);
+        Box box3 = new Box(234.43, 234.45, 11.34);
+//        box1.setData(34.44, 34.55, 0.99);
+        box1.setDepth(23.35);
+        box1.setHeight(23.35);
+        box1.setWidth(23.35);
+        double result = box1.getVolume();
+        System.out.println(box1.getVolume());
+        Box[] boxes = new Box[]{box1, box2, box3};
+        for (int i = 0; i < boxes.length; i++) {
+            System.out.println(boxes[i].toString());
+        }
     }
 }
+
